769a1749d9ac71156c0a33f35b86b7d8e11fd387
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-02-27 22:41:44 +0530

    connecting Firebase backend
    make  environment for firebase with help of some AI bots

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.blue, // Global primary color
+        primaryColor: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
       ),
       home: const LoginPage(),
