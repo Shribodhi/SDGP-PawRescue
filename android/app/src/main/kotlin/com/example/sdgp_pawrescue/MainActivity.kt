3cbae5910a8a87e6b67e4a7ec9f6940625badeb0
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-02-27 15:26:03 +0530

    Started on Travel recommender feature UI
    I had to delete may two branches because I face lot of issues preparing environment in Android studio. So i decided to work with VS code. for this commit creating travel recommender dart file is the only update.

--- android/app/src/main/kotlin/com/example/sdgp_pawrescue/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sdgp_pawrescue/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.sdgp_pawrescue
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Methu\Documents\GitHub\SDGP-PawRescue"
+export "FLUTTER_ROOT=D:\1 Samadhi UNI\SDGP\SDGP-PawRescue\.dart_tool\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\1 Samadhi UNI\SDGP GIT 2\SDGP-PawRescue"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
