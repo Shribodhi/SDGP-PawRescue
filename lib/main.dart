b6992dbeda96cd70dec67f48adbbce55093473cf
Thamara Perera <thamaraperera1039@gmail.com>
2025-03-17 23:50:54 +0530

    Added firebase
    Added firebase , so many errors occured during the process. now i ve fixed them. app running smoothly.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 
-void main() {
+void main() async {
+
+WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: FirebaseOptions(apiKey: 'AIzaSyDFXl7f13s8d8LwcSQdbQMcGLMOBpwjmvc',
+appId: '1:197972141258:android:f5c39199b230c150d40033', 
+messagingSenderId: '197972141258',
+projectId: 'paw-rescue-a8681'));
+
+
   runApp(const MyApp());
 }
 
